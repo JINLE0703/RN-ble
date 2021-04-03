7781bee43e068f95a963ef9134589dc883a2981a
Jinle <jinle0703@foxmail.com>
2021-04-03 18:05:49 +0800

    init bleModule

--- src/index.js
+++ src/index.js
@@ -1,15 +1,205 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Alert,
+} from 'react-native';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
+import BleModule from './modules/BleModule';
+
+const Ble = new BleModule();
+global.Ble = Ble;
+
+export default function App() {
+  const [bleList, setBleList] = useState([]); // 扫描的蓝牙列表
+  const [isConnected, setIsConnected] = useState(false); // 连接状态
+  const [receiveData, setReceiveData] = useState([]); // 接收的数据的缓存列表
+
+  /**
+   * 初始化绑定监听器
+   */
+  useEffect(() => {
+    Ble.start(); // 初始化蓝牙模块
+    const updateStateListener = Ble.addListener(
+      'BleManagerDidUpdateState',
+      handleUpdateState,
+    );
+    const stopScanListener = Ble.addListener(
+      'BleManagerStopScan',
+      handleStopScan,
+    );
+    const discoverPeripheralListener = Ble.addListener(
+      'BleManagerDiscoverPeripheral',
+      handleDiscoverPeripheral,
+    );
+    const connectPeripheralListener = Ble.addListener(
+      'BleManagerConnectPeripheral',
+      handleConnectPeripheral,
+    );
+    const disconnectPeripheralListener = Ble.addListener(
+      'BleManagerDisconnectPeripheral',
+      handleDisconnectPeripheral,
+    );
+    const updateValueListener = Ble.addListener(
+      'BleManagerDidUpdateValueForCharacteristic',
+      handleUpdateValue,
+    );
+    return () => {
+      updateStateListener.remove();
+      stopScanListener.remove();
+      discoverPeripheralListener.remove();
+      connectPeripheralListener.remove();
+      disconnectPeripheralListener.remove();
+      updateValueListener.remove();
+      if (isConnected) {
+        Ble.disconnect();
+      }
+    };
+  }, []);
+
+  /**
+   * 处理蓝牙状态变化
+   * @args state: on/off
+   */
+  const handleUpdateState = args => {
+    console.log('BleManagerDidUpdateState', args);
+  };
+
+  /**
+   * 处理扫描结束
+   */
+  const handleStopScan = () => {
+    console.log('BleManagerStopScan');
+    // 获取扫描结果数组
+    Ble.getDiscoveredPeripherals().then(res => {
+      setBleList(res);
+    });
+  };
+
+  /**
+   * 扫描发现新的外围设备
+   * @param {id, name, rssi} args
+   */
+  const handleDiscoverPeripheral = args => {
+    console.log('BleManagerDiscoverPeripheral', args);
+  };
+
+  /**
+   * 处理连接外围设备
+   */
+  const handleConnectPeripheral = args => {
+    console.log('BleManagerConnectPeripheral', args);
+  };
+
+  /**
+   * 处理断开外围设备
+   * @param {*} args
+   */
+  const handleDisconnectPeripheral = args => {
+    console.log('BleManagerDisconnectPeripheral', args);
+  };
+
+  /**
+   * 处理接收新数据
+   * @param {*} data
+   */
+  const handleUpdateValue = data => {
+    console.log('BleManagerDidUpdateValueForCharacteristic', data);
+    const {value} = data;
+    let str = Ble.byteToString(value);
+    setReceiveData(prevData => [...prevData, str]);
+  };
+
+  /**
+   * 扫描外围设备
+   */
+  const scan = () => {
+    Ble.scan();
+  };
+
+  /**
+   * 连接设备
+   * @param {*}} id
+   */
+  const connect = id => {
+    Ble.connect(id).then(info => {
+      setIsConnected(true);
+      Ble.startNotification();
+      Ble.writeWithoutResponse('start');
+    });
+  };
+
+  /**
+   * 断开设备
+   */
+  const disconnect = () => {
+    Ble.disconnect();
+    setIsConnected(false);
+  };
+
+  /**
+   * 读数据
+   */
+  const read = () => {
+    Ble.read().then(readData => {
+      Alert.alert(readData);
+      console.log(readData);
+    });
+  };
+
+  const renderItem = item => {
+    let data = item.item;
     return (
       <View>
-        <Text>hello</Text>
+        <Text>id: {data.id}</Text>
+        <Text>name: {data.name}</Text>
+        <Text>rssi: {data.rssi}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            connect(data.id);
+          }}>
+          <View style={styles.btn}>
+            <Text>connect</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={disconnect}>
+          <View style={styles.btn}>
+            <Text>disconnect</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
-  }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={scan}>
+        <View style={styles.btn}>
+          <Text>scan</Text>
+        </View>
+      </TouchableOpacity>
+      <FlatList data={bleList} renderItem={renderItem} />
+      <Text>{receiveData.map(item => item)}</Text>
+      <TouchableOpacity onPress={read}>
+        <View style={styles.btn}>
+          <Text>read</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  btn: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ddd',
+  },
+});
---
+++ src/modules/BleModule.js
@@ -0,0 +1,303 @@
+import {Platform, NativeModules, NativeEventEmitter} from 'react-native';
+import BleManager from 'react-native-ble-manager';
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
+export default class BleModule {
+  constructor() {
+    this.isConnecting = false; // 蓝牙是否处于连接状态
+    this.bleState = 'off'; // 蓝牙打开状态
+    this.peripheralId = null; // 当前连接设备id
+    // this.initUUID();
+    this.serviceUUID = null;
+    this.characteristicUUID = null;
+  }
+
+  /**
+   * 添加监听器
+   * @param {*} event 监听事件
+   * @param {*} fnc 回调函数
+   * @returns
+   * BleManagerStopScan：扫描结束监听
+   * BleManagerDiscoverPeripheral：扫描到一个新设备
+   * BleManagerDidUpdateState：蓝牙状态改变
+   * BleManagerDidUpdateValueForCharacteristic：接收到新数据
+   * BleManagerConnectPeripheral：蓝牙设备已连接
+   * BleManagerDisconnectPeripheral：蓝牙设备已断开连接
+   */
+  addListener(event, fnc) {
+    return bleManagerEmitter.addListener(event, fnc);
+  }
+
+  /**
+   * 初始化蓝牙模块
+   */
+  start() {
+    BleManager.start({showAlert: false})
+      .then(() => {
+        this.checkState();
+        console.log('Init BLE success');
+      })
+      .catch(err => {
+        console.log('Init BLE fail: ', err);
+      });
+  }
+
+  /**
+   * 强制检查蓝牙状态，会触发 BleManagerDidUpdateState 事件
+   */
+  checkState() {
+    BleManager.checkState();
+  }
+
+  /**
+   * 扫描可用设备，5秒结束
+   * 触发 BleManagerDiscoverPeripheral 事件
+   * @returns
+   */
+  scan() {
+    return new Promise((resolve, reject) => {
+      BleManager.scan([], 5, true)
+        .then(() => {
+          console.log('Scan start');
+          resolve();
+        })
+        .catch(err => {
+          console.log('Scan start fail: ', err);
+          reject();
+        });
+    });
+  }
+
+  /**
+   * 停止扫描
+   */
+  stopScan() {
+    BleManager.stopScan()
+      .then(() => {
+        console.log('Scan Stop');
+      })
+      .catch(err => {
+        console.log('Scan stop fail: ', err);
+      });
+  }
+
+  /**
+   * 获取扫描结果
+   * @returns 扫描外围设备数组
+   */
+  getDiscoveredPeripherals() {
+    return new Promise((resolve, reject) => {
+      BleManager.getDiscoveredPeripherals([])
+        .then(peripheralsArray => {
+          console.log('getDiscoveredPeripherals: ', peripheralsArray);
+          resolve(peripheralsArray);
+        })
+        .catch(err => {
+          console.log('getDiscoveredPeripherals fail: ', err);
+        });
+    });
+  }
+
+  /**
+   * 连接设备
+   * @param {*} id
+   * @returns
+   */
+  connect(id) {
+    this.isConnecting = true; // 当前蓝牙正在连接
+    return new Promise((resolve, reject) => {
+      BleManager.connect(id)
+        .then(() => {
+          console.log('Connect success');
+          return BleManager.retrieveServices(id); // 扫描连接设备服务和特征
+        })
+        .then(peripheralInfo => {
+          console.log('Connect Peripheral info: ', peripheralInfo);
+          this.peripheralId = peripheralInfo.id;
+          this.getUUID(peripheralInfo); // 获取UUID
+          this.isConnecting = false; // 蓝牙连接结束
+          resolve(peripheralInfo);
+        })
+        .catch(err => {
+          console.log('Connect fail: ', err);
+          this.isConnecting = false; // 蓝牙连接结束
+          reject(err);
+        });
+    });
+  }
+
+  /**
+   * 断开外围设备
+   */
+  disconnect() {
+    BleManager.disconnect(this.peripheralId)
+      .then(() => {
+        console.log('disconnect success');
+        this.peripheralId = null;
+        this.serviceUUID = null;
+        this.characteristicUUID = null;
+      })
+      .catch(err => {
+        console.log('disconnect fail: ', err);
+      });
+  }
+
+  /**
+   * 获取UUID
+   * Notify、Read、Write、WriteWithoutResponse的serviceUUID和characteristicUUID
+   */
+  getUUID(peripheralInfo) {
+    for (let item of peripheralInfo.characteristics) {
+      item.service = this.fullUUID(item.service);
+      item.characteristic = this.fullUUID(item.characteristic);
+      // 寻找目标服务
+      if (
+        item.properties.Notify === 'Notify' &&
+        item.properties.Read === 'Read' &&
+        item.properties.WriteWithoutResponse === 'WriteWithoutResponse'
+      ) {
+        this.serviceUUID = item.service;
+        this.characteristicUUID = item.characteristic;
+      }
+    }
+  }
+
+  /**
+   * 格式化UUID
+   * @param {*} uuid
+   * @returns
+   */
+  fullUUID(uuid) {
+    if (uuid.length === 4) {
+      return '0000' + uuid.toUpperCase() + '-0000-1000-8000-00805F9B34FB';
+    }
+    if (uuid.length === 8) {
+      return uuid.toUpperCase() + '-0000-1000-8000-00805F9B34FB';
+    }
+    return uuid.toUpperCase();
+  }
+
+  /**
+   * 开启监听通知
+   * @returns
+   */
+  startNotification() {
+    return new Promise((resolve, reject) => {
+      BleManager.startNotification(
+        this.peripheralId,
+        this.serviceUUID,
+        this.characteristicUUID,
+      )
+        .then(() => {
+          console.log('Notification started');
+          resolve();
+        })
+        .catch(err => {
+          console.log('Notification fail: ', err);
+          reject(err);
+        });
+    });
+  }
+
+  /**
+   * 停止监听通知
+   */
+  stopNotification() {
+    BleManager.stopNotification(
+      this.peripheralId,
+      this.serviceUUID,
+      this.characteristicUUID,
+    )
+      .then(() => {
+        console.log('Notification stop success');
+      })
+      .catch(err => {
+        console.log('Notification stop fail: ', err);
+      });
+  }
+
+  /**
+   * 读取数据
+   * @returns
+   */
+  read() {
+    return new Promise((resolve, reject) => {
+      BleManager.read(
+        this.peripheralId,
+        this.serviceUUID,
+        this.characteristicUUID,
+      )
+        .then(readData => {
+          const parsedData = this.byteToString(readData);
+          console.log('Read: ', parsedData);
+          resolve(parsedData);
+        })
+        .catch(err => {
+          console.log('Read fail: ', err);
+          reject(err);
+        });
+    });
+  }
+
+  /**
+   * 写数据进设备，没有响应
+   * @param {*} data
+   * @returns
+   */
+  writeWithoutResponse(data) {
+    return new Promise((resolve, reject) => {
+      BleManager.writeWithoutResponse(
+        this.peripheralId,
+        this.serviceUUID,
+        this.characteristicUUID,
+        this.stringToByte(data),
+      )
+        .then(() => {
+          console.log('Write success: ', data);
+          resolve();
+        })
+        .catch(err => {
+          console.log('Write fail:', data);
+          reject(err);
+        });
+    });
+  }
+
+  /**
+   * 请求激活蓝牙
+   */
+  enableBluetooth() {
+    BleManager.enableBluetooth()
+      .then(() => {
+        console.log('The bluetooth is already enabled or the user confirm');
+      })
+      .catch(err => {
+        console.log('The user refuse to enable bluetooth: ', err);
+      });
+  }
+
+  /**
+   * ASCII转字符串
+   */
+  byteToString(arr) {
+    let str = '';
+    arr.map(item => {
+      str += String.fromCharCode(item);
+    });
+    return str;
+  }
+
+  /**
+   * 字符串转ASCII
+   */
+  stringToByte(str) {
+    let arr = [];
+    for (let char of str) {
+      arr.push(char.charCodeAt());
+    }
+    console.log(arr);
+    return arr;
+  }
+}
